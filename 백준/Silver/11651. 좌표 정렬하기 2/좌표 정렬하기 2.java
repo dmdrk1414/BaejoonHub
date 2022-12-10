28cf7b0da47a38fd1e3f3f250456dfca22ab112a
dmdrk1414 <seungchan141414@gmail.com>
2022-12-10 10:28:41 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1564 ms, Memory: 66896 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -21,7 +21,7 @@ public class Main {
             @Override
             public int compare(int[] o1, int[] o2) {
                 if (o1[1] == o2[1]) {
-                    return o1[0] - o2[0];
+                    return Integer.compare(o1[0], o2[0]);
                 }
                 return Integer.compare(o1[1], o2[1]);
             }
