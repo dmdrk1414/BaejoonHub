cdd515e1ba8f91fe01517f5fe6d3ec1864397b6c
dmdrk1414 <seungchan141414@gmail.com>
2023-01-10 10:28:19 +0900

    [Bronze IV] Title: 방학 숙제, Time: 224 ms, Memory: 17964 KB -BaekjoonHub

---
+++ 백준/Bronze/5532. 방학 숙제/방학 숙제.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static double bigger(double a, double b) {
+		if(a>b)
+			return a;
+		else
+			return b;
+	}
+	
+	public static void main(String[] args) {
+		int L;
+		double A,B,C,D;
+		Scanner scann = new Scanner(System.in);
+		L=scann.nextInt();
+		A=scann.nextDouble();
+		B=scann.nextDouble();
+		C=scann.nextDouble();
+		D=scann.nextDouble();
+		int homework = (int) Math.ceil(bigger(A/C,B/D));
+		System.out.println(L-homework);
+	}
+
+}
