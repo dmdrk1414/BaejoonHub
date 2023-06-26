f1517a17ef026659f1ba66deb37b57031709fef8
dmdrk1414 <seungchan141414@gmail.com>
2023-06-26 11:44:41 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 708 ms, Memory: 142048 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    static ArrayList<Integer>[] graph; // 그래프
+    static  boolean [] visited; // 방문 배열
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            graph[start].add(end);
+            graph[end].add(start);
+        }
+
+        int count = 0;  // 연결 요소 개수
+
+        for (int i = 0; i < n + 1; i++) {
+            if (!visited[i]) {
+                count++;
+                DFS(i);
+            }
+        }
+
+        System.out.println(count - 1);
+    }
+
+    private static void DFS(int v) {
+        if(visited[v]) return; // 방문노드와 탐색 노드가 같으면
+
+        visited[v] = true;
+        for (int i: graph[v] ) {
+            if (!visited[i]) {
+                DFS(i);
+            }
+        }
+    }
+}
