6567d498f6ae27d639d406f9feb45a80acdaf641
박승찬(Victory) <seungchan141414@gmail.com>
2024-08-05 22:08:30 +0900

    [Gold I] Title: 수열과 쿼리 16, Time: 556 ms, Memory: 51760 KB -BaekjoonHub

--- 백준/Gold/14428. 수열과 쿼리 16/수열과 쿼리 16.java
+++ 백준/Gold/14428. 수열과 쿼리 16/수열과 쿼리 16.java
@@ -1,82 +1,128 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
+
+/*
+5
+5 4 3 2 1
+6
+2 1 3
+2 1 4
+1 5 3
+2 3 5
+1 4 3
+2 3 5
+
+ */
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int N, M;
+    static int[] arr;
+    static int[] tree;
+    static StringBuilder sb;
 
-	static int[] arr, tree;
-	static int n;
+    private static void input() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		arr = new int[n+1];
-		tree = new int[4*n];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		arr[0] = Integer.MAX_VALUE;
-		for(int i=1; i<=n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		init(1, n, 1);
-		StringBuilder sb = new StringBuilder();
-		int m = Integer.parseInt(br.readLine());
-		for(int i=0; i<m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int op = Integer.parseInt(st.nextToken());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			if(op == 1) {
-				arr[a] = b;
-				update(1, n, 1, a);
-			}else {
-				sb.append(getMin(1, n, 1, a, b)+"\n");
-			}
-		}
-		
-		System.out.println(sb.toString());
-		
-	}
-    
-	static int init(int s, int e, int node) {
-		if(s == e) return tree[node] = s;
-		
-		int mid = (s+e)/2;
-		int left = init(s, mid, node*2);
-		int right =  init(mid+1, e, node*2+1);
-		return tree[node] =  getIndex(left,right); 
-	}
-	
-	static int update(int s, int e, int node, int idx) {
-		if(s > idx || e < idx ) return tree[node];
-		if(s == e)  return tree[node] = idx;
-		
-		int mid = (s+e)/2;
-		int left = update(s, mid, node*2, idx);
-		int right =  update(mid+1, e, node*2+1, idx);
-		return tree[node] = getIndex(left,right);
-	}
-	
-	static int getMin(int s, int e, int node, int l, int r) {
-		if(r < s || l > e ) return 0;
-		if(l <= s && e <= r) {
-			return tree[node];
-		}
-		
-		int mid = (s+e)/2;
-		int left = getMin(s, mid, node*2, l, r);
-		int right =  getMin(mid+1, e, node*2+1, l, r);
-		return getIndex(left, right);
-	}
-	
-	static int getIndex(int left, int right) {
-		if(arr[left] == arr[right]) return getMin(left, right);
-		else if(arr[left] < arr[right]) return left;
-		else return right;
-	}
-	
-	static int getMin(int left, int right) {
-		if(left < right) {
-			return left;
-		}else return right;
-	}	
+        arr = new int[N + 1];
+        tree = new int[N * 4];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        arr[0] = Integer.MAX_VALUE;
+
+        init(1, 1, N);
+    }
+
+    private static int init(final int node, final int nodeLeft, final int nodeRight) {
+        if(nodeLeft == nodeRight) {
+            return tree[node] = nodeLeft;
+        }
+
+        int mid = nodeLeft + (nodeRight - nodeLeft) / 2;
+        int left = init(node * 2, nodeLeft, mid);
+        int right = init(node * 2 + 1, mid + 1, nodeRight);
+
+        return tree[node] = getIndex(left, right);
+    }
+
+    private static void pro() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int query = Integer.parseInt(st.nextToken());
+
+            if (query == 1) {
+                int index = Integer.parseInt(st.nextToken());
+                int update = Integer.parseInt(st.nextToken());
+                arr[index] = update;
+                updateRec(index, 1, 1, N);
+            } else if (query == 2) {
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                sb.append(getRec(a, b, 1, 1, N)).append("\n");
+            }
+        }
+
+        bw.write(sb.toString());
+    }
+
+    private static int updateRec(final int index, final int node, final int nodeLeft, final int nodeRight) {
+        if(index < nodeLeft || nodeRight < index){
+            return tree[node];
+        }
+
+        if(nodeLeft == nodeRight) {
+            return tree[node];
+        }
+
+
+        int mid = nodeLeft + (nodeRight - nodeLeft) / 2;
+        int leftVal = updateRec(index, node * 2, nodeLeft, mid);
+        int rightVal = updateRec(index, node * 2 + 1, mid + 1, nodeRight);
+
+        return tree[node] = getIndex(leftVal, rightVal);
+    }
+
+    private static int getRec(final int left, final int right, final int node, final int nodeLeft, final int nodeRight) {
+        if(right < nodeLeft || nodeRight < left){
+            return 0;
+        }
+
+        if(left <= nodeLeft && nodeRight <= right) {
+            return tree[node];
+        }
+
+        int mid = nodeLeft + (nodeRight - nodeLeft) / 2;
+        int leftVal = getRec(left, right, node * 2, nodeLeft, mid);
+        int rightVal = getRec(left, right, node * 2 + 1, mid + 1, nodeRight);
+
+        return getIndex(leftVal, rightVal);
+    }
+
+    static int getIndex(int left, int right) {
+        if(arr[left] == arr[right]) return Math.min(left, right);
+
+        else if(arr[left] < arr[right]) return left;
+
+        else return right;
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        pro();
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
